5b308072d22f29eda78847d75f2f243896b779aa
Abhishek <abhishek.pandey@ucertify.com>
2023-03-13 11:07:30 +0530

    commoit

--- .github/workflows/index.php
+++ .github/workflows/index.php
@@ -1,4 +1,3 @@
-$num1 = 10;
-$num2 = 20;
-$sum = $num1 + $num2;
-echo "The sum of $num1 and $num2 is: $sum";
+<?php
+
+phpinfo();
--- index.php
+++
@@ -1,4 +0,0 @@
-$num1 = 10;
-$num2 = 20;
-$sum = $num1 + $num2;
-echo "The sum of $num1 and $num2 is: $sum";
